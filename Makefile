5f2073a47ca767ebb36a550a2af2afa6140980cf
SametSisartenep <rodrigosloop@gmail.com>
2016-07-29 00:27:18 +0200

    Syntax style updated (Based on Linux C Style).

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 CPPFLAGS:=-D_FORTIFY_SOURCE=2
 CFLAGS:=-Wall -Wextra -Werror -fno-exceptions -fstack-protector-strong -Wl,-z,relro -Wl,-z,now -Wformat-security -Wpointer-arith -Wformat-nonliteral -Winit-self -Wl,-O1a -Wl,--no-undefined -Wl,--build-id=sha1 -Wpedantic -pedantic --param=ssp-buffer-size=4 -s -O2
 CC:=$(CC)
+SOURCE=showip.c
+BIN=showip
 
-showip: showip.c
+$(BIN): $(SOURCE)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -o $@ $<
 
-all: showip
+all: $(BIN)
 
-.PHONY: all
+clean:
+	rm -rfv $(BIN)
+
+.PHONY: all clean
--- showip.c
+++ showip.c
@@ -7,46 +7,42 @@
 
 int main (int argc, char **argv)
 {
-  struct addrinfo hints = {0}, *result, *node_ptr;
-  int status;
-  char ipstr[INET6_ADDRSTRLEN];
+	struct addrinfo hints = {0}, *result, *node_ptr;
+	int status;
+	char ipstr[INET6_ADDRSTRLEN];
 
-  if (argc != 2)
-  {
-    fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
-    return 1;
-  }
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
+		return 1;
+	}
 
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
-  if ((status = getaddrinfo(argv[1], NULL, &hints, &result)) != 0)
-  {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    return 2;
-  }
+	if ((status = getaddrinfo(argv[1], NULL, &hints, &result)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		return 2;
+	}
 
-  printf("IP addresses for %s\n\n", argv[1]);
+	printf("IP addresses for %s\n\n", argv[1]);
 
-  for (node_ptr = result; node_ptr; node_ptr = node_ptr->ai_next)
-  {
-    char *ipver;
+	for (node_ptr = result; node_ptr; node_ptr = node_ptr->ai_next) {
+		char *ipver;
 
-    switch (node_ptr->ai_family)
-    {
-      case AF_INET:
-        ipver = "IPv4";
-        break;
-      case AF_INET6:
-        ipver = "IPv6";
-        break;
-    }
+		switch (node_ptr->ai_family) {
+			case AF_INET:
+				ipver = "IPv4";
+				break;
+			case AF_INET6:
+				ipver = "IPv6";
+				break;
+		}
 
-    getnameinfo(node_ptr->ai_addr, node_ptr->ai_addrlen, ipstr, sizeof ipstr, NULL, 0, NI_NUMERICHOST);
-    printf("\t%s:\t%s\n", ipver, ipstr);
-  }
+		getnameinfo(node_ptr->ai_addr, node_ptr->ai_addrlen, ipstr, sizeof ipstr, NULL, 0, NI_NUMERICHOST);
+		printf("\t%s:\t%s\n", ipver, ipstr);
+	}
 
-  freeaddrinfo(result);
+	freeaddrinfo(result);
 
-  return 0;
+	return 0;
 }
